ea8b59905a818c98aa615ce49e18c6494b802b95
jiahaoc <jiahaoc@vmware.com>
2021-01-08 17:57:48 +0800

    add kubefate test cases

---
+++ ansible/roles/kubefate/files/kubefate-e2e/fate_deploy_test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+dir=$(cd $(dirname $0) && pwd)
+source $dir/color.sh
+
+source $dir/common.sh
+
+if kubefate_install; then
+    loginfo "kubefate install success"
+else
+    exit 1
+fi
+
+set_host
+
+if check_kubefate_version; then
+    loginfo "kubefate CLI ready"
+else
+    exit 1
+fi
+
+set_cluster_image
+
+if cluster_install; then
+    loginfo "cluster install success"
+else
+    exit 1
+fi
+
+if cluster_update; then
+    loginfo "cluster update success"
+else
+    exit 1
+fi
+
+if check_cluster_status; then
+    loginfo "check cluster status success"
+else
+    exit 1
+fi
+
+if cluster_delete; then
+    loginfo "cluster delete success"
+else
+    exit 1
+fi
+loginfo "Cluster CURD test Success!"
+
+kubefate_uninstall
+
+clean_host
+
+loginfo "fate_deploy_test done."
+exit 0
